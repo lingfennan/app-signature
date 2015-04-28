1af65d27fb1521c189a8ee38145bac66f650b9f9
Ruian Duan <duanruian@gmail.com>
2015-04-28 11:28:13 -0400

    Added the complete gpl list from android arsenal. Updated README for icon-matching.

---
+++ data/sdk_lib/android_arsenal_lib/commands.sh
@@ -0,0 +1,13 @@
+# download the page
+wget https://android-arsenal.com/free
+# get the app links
+cat free | grep -o '<a href=[^>]*>[^<]*</a>' | grep \"/details > free_2
+# download each app link
+cat android_arsenal_list | while read line; do OUTFILE=$(echo $line | grep -oE "[^/]+$"); curl $line > android_arsenal_sdk_dst/$OUTFILE; done
+# find GPL licenses
+grep '[^L]GPL' *
+
+# the two other pages
+wget https://android-arsenal.com/demo
+wget https://android-arsenal.com/paid
+
