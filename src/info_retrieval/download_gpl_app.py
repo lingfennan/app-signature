0a8e420b7e5fdaaa300bb0b6e206f369cff2e3e5
Ruian Duan <duanruian@gmail.com>
2015-08-25 00:01:57 -0400

    Download android arsenal projects done. Starting to process F-droid apps.

--- src/info_retrieval/download_gpl_app.py
+++ src/info_retrieval/download_gpl_app.py
@@ -1,2 +1,32 @@
+import subprocess
+import re
 
+# Download GPL app from android arsenal.
+def wget_and_store(url, outfile):
+    p1 = subprocess.Popen(['wget', '-O', outfile, url], stdout=subprocess.PIPE)
+    output, error = p1.communicate()
+    return error
 
+def download_from_android_arsenal(inlist, outdir):
+    for link in inlist:
+        print link
+        p1 = subprocess.Popen(["curl", link], stdout=subprocess.PIPE)
+        output, error = p1.communicate()
+        # Get the group https.*zip
+        pattern = r'(?:<a class="actionLink" title="Download sources" href=")(https.*zip)">'
+        p = re.compile(pattern)
+        m = p.search(output)
+        if m:
+            url = m.groups()[0]
+            outfile = outdir + '-'.join(link.split('/')[-2:]) + '-master.zip'
+            print url
+            wget_and_store(url, outfile)
+
+# Download GPL app from F-droid.
+
+# Download Google Play app.
+
+IN_FILE = '../../data/sdk_lib/android_arsenal_lib/gpl_list'
+OUT_DIR = '../../data/sdk_lib/android_arsenal_lib/projects/'
+inlist = filter(bool, open(IN_FILE, 'r').read().split('\n'))
+download_from_android_arsenal(inlist, OUT_DIR)
--- src/utils/apk_contents/parse_manifest.py
+++ src/utils/apk_contents/parse_manifest.py
@@ -1,1 +1,27 @@
+import xml
+
+class ApkProcessor(object):
+    def unpack(self):
+        return
+
+    def parseManifest(self):
+        """ Parse and store all the interesting information from the manifest.xml. If files are small, store them
+        separately.
+        """
+        # extract package name
+        # extract application node
+        # extract activity node
+        # extract service node
+        # extract permission
+        # extract ...
+        return
+
+    def extractIcon(self):
+        return
+
+    def extractName(self):
+        return
+
+    def extractImage(self):
+        return
 
